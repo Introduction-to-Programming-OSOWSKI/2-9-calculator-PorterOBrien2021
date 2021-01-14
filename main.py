c576086e04143ff6bacf17185f5c2a82071b72f2
PorterOBrien2021 <824085@mystma.org>
2021-01-14 20:05:49 +0000

    calc

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def calculate(x, y, z):
+    if x == "add":
+        return y + z
+    elif x == "subtract":
+        return y - z
+    elif x == "multiply":
+        return y * z
+    else:
+        return y / 2
+print (calculate("subtract", 5, 5))
